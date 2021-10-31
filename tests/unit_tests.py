64611fde3d4aae80f87a417e0fd0cd85e3f2249c
Pranavi <vasapranavi@gmail.com>
2021-10-31 21:30:06 +0000

    Added unit test cases for positive and negative scenarios.

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,18 +1,38 @@
 import unittest
-
+from scripts import main
 
 class MyTestCase(unittest.TestCase):
     def test_dog_picture(self):
-        self.assertEqual(True, False)
+        try:
+            pict = main.Image("dog.jpg");
+            actual = main.get_prediction(pict);
+            self.assertEqual("golden_retriever", actual.get('response'))
+        except Exception as exp:
+            self.fail(exp);
 
     def test_cat_picture(self):
-        self.assertEqual(True, False)
+        try:
+            pict = main.Image("cat.jfif");
+            actual = main.get_prediction(pict);
+            self.assertEqual("cat", actual.get('response'))
+        except Exception as exp:
+            self.fail(exp);
 
     def test_lampshade(self):
-        self.assertEqual(True, False)
+        try:
+            pict = main.Image("lampshade.jpg");
+            actual = main.get_prediction(pict);
+            self.assertEqual("lampshade", actual.get('response'))
+        except Exception as exp:
+            self.fail(exp);
 
     def test_invalidImage(self):
-        self.assertEqual(True, False)
+        try:
+            pict = main.Image("nonexistent.jpg");
+            actual = main.get_prediction(pict);
+            self.fail("Should throw exception")
+        except Exception as exp:
+            self.assertEqual("Not found", exp);
 
 if __name__ == '__main__':
     unittest.main()
